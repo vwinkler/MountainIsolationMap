3a5ce75d66432c67df380e6a967ceddc6dbaeb11
Vincent Winkler <vincent.winkler@student.kit.edu>
2020-08-09 12:37:41 +0200

    Implement caching

--- makeMap.py
+++ makeMap.py
@@ -1,12 +1,29 @@
+import pickle
 import wikidataDownload
 from map import Map
 
+cacheFileName = "mountainsCache.pkl"
+
+def loadMountains():
+    return pickle.load(open(cacheFileName, "rb"))
+
+def storeMountains(mountains):
+    pickle.dump(mountains, open(cacheFileName, "wb"))
 
 def main():
-    mountains = wikidataDownload.loadMountains()
+    try:
+        print("Load mountains mountains from cache..")
+        mountains = loadMountains()
+    except:
+        print("Could not load mountains from cache.")
+        print("Downloading mountains from Wikidata...")
+        mountains = wikidataDownload.loadMountains()
+        storeMountains(mountains)
+
     for mountain in mountains:
         pattern = "{} ({} {}): {} meters"
         print(pattern.format(mountain.name, mountain.latitude, mountain.longtitude, mountain.elevation))
+    
     map = Map()
     for mountain in mountains:
         map.addMountain(mountain)
